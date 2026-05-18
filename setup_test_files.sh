324d4fd5242e8c0aaa8f2a4dfc06f1dd46dcfffd
blitterated <blitterated@protonmail.com>
2026-05-17 22:53:48 -0500

    Use env vars from env.sh

--- setup_test_files.sh
+++ setup_test_files.sh
@@ -1,15 +1,18 @@
 #! /opt/homebrew/bin/bash
 
+# Same as `set -eu`
+set -o errexit -o nounset
+
 source "./env.sh"
 
 BACKUP_DIR="test_dir_backups"
 
 # Check if test directory exists. If so, back it up and delete the tree
-if [ -d "${test_dir}" ]; then
+if [ -d "${TEST_DIR}" ]; then
   TIMESTAMP=$(date +%s)
-  BACKUP_FILENAME="${test_dir_name}_${TIMESTAMP}.tgz"
+  BACKUP_FILENAME="${TEST_DIR_NAME}_${TIMESTAMP}.tgz"
 
-  gtar -czvf "${BACKUP_FILENAME}" "${test_dir_name}"
+  gtar -czvf "${BACKUP_FILENAME}" "${TEST_DIR_NAME}"
   mv "${BACKUP_FILENAME}" "${BACKUP_DIR}"
   rm -rf "${TEST_DIR_NAME}"
 
@@ -17,17 +20,17 @@ if [ -d "${test_dir}" ]; then
 fi
 
 # Create the folder structure
-mkdir -p "${NVIM_CONFIG_ROOT}/mise"
+mkdir -p "${NVIM_MISE_GLOBAL_CONFIG_ROOT}"
 mkdir -p "${NVIM_TOOL_INSTALL_ROOT}"
 
 # Create README for config_root
-cat << EOF > "${TEST_DIR}/config_root/README.txt"
+cat << EOF > "${NVIM_CONFIG_ROOT}/README.txt"
 When using MISE_CEILING_PATHS, mise will search directories under the directory specified as the ceiling for config files, but _NOT_ the ceiling direct itself.
 EOF
 
 # Create mise config file
-#cat << 'EOF' > "${test_dir}/config_root/mise/mise.neovim.toml"
-cat << 'EOF' > "${test_dir}/config_root/mise/mise.toml"
+#cat << 'EOF' > "${NVIM_MISE_GLOBAL_CONFIG_ROOT}/mise.neovim.toml"
+ cat << 'EOF' > "${NVIM_MISE_GLOBAL_CONFIG_ROOT}/mise.toml"
 [tools]
 node = "lts"
 "aqua:LuaLS/lua-language-server" = "latest"
