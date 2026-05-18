16ddf4292fef713f4e2369f3635163441093c326
blitterated <blitterated@protonmail.com>
2026-05-18 00:04:50 -0500

    Narrow down how mise env vars work

--- env.sh
+++ env.sh
@@ -4,6 +4,20 @@
 #
 # Source this file. Don't just execute it.
 
+# Start clean with custom env vars
+unset CUR_WRK_DIR
+unset NVIM_CONFIG_ROOT
+unset NVIM_MISE_GLOBAL_CONFIG_ROOT
+unset NVIM_TOOL_INSTALL_ROOT
+unset TEST_DIR
+unset TEST_DIR_NAME
+
+# Start clean with mise env vars
+unset MISE_CEILING_PATHS
+unset MISE_ENV
+unset MISE_GLOBAL_CONFIG_ROOT
+unset MISE_INSTALLS_DIR
+
 # export NVIM_CONFIG_ROOT="${HOME}/.config/nvim"
 # export NVIM_TOOL_INSTALL_ROOT="${HOME}/.local/share/nvim"
 
@@ -18,16 +32,25 @@ export NVIM_MISE_GLOBAL_CONFIG_ROOT="${NVIM_CONFIG_ROOT}/mise/"
 export NVIM_TOOL_INSTALL_ROOT="${TEST_DIR}/installs"
 
 # config file must be named config.neovim.toml or mise.neovim.toml
-export MISE_ENV="neovim"
+#export MISE_ENV="neovim"
 
 # force the global config to live under Neovim's config tree.
-#export MISE_GLOBAL_CONFIG_ROOT="${nvim_config_root}/mise/"
-export MISE_GLOBAL_CONFIG_ROOT="nvim_mise_global_config_root"
+export MISE_GLOBAL_CONFIG_ROOT="NVIM_MISE_GLOBAL_CONFIG_ROOT"
 
 # cleaner install of neovim related tools (LSPs and deps) to a dir under neovim's data dir.
 export MISE_INSTALLS_DIR="${NVIM_TOOL_INSTALL_ROOT}/mise/installs/"
 
 # prevents merging any config files found above nvim_config_root.
 #export MISE_CEILING_PATHS="${NVIM_CONFIG_ROOT}"
+#export MISE_CEILING_PATHS="\"${NVIM_CONFIG_ROOT}\""
+#export MISE_CEILING_PATHS="NVIM_CONFIG_ROOT"
 
-export MISE_CEILING_PATHS="nvim_mise_global_config_root"
+
+
+
+
+
+
+
+
+
