ee23a803addb13c3963426c78daa1e68f082362c
blitterated <blitterated@protonmail.com>
2026-05-21 01:52:12 -0500

    Consolidate scripts in to single tool script: devtool

--- bin/show_mise_cfg.sh
+++
@@ -1,9 +0,0 @@
-#! /opt/homebrew/bin/bash
-
-# Import mise env vars for an isolated environment for Neovim.
-source "../lib/env.dev.sh"
-
-env | ggrep "MISE_"
-
-# Show mise's configuration hierarchy
-mise config ls
